123e240b7e8912ab5fb9ab95feec403f5fb9f68e
RomanDV22 <rdevita@outlook.com.ar>
2026-05-14 23:38:00 +0000

    Added distributed monitoring agent and requirements

---
+++ agents/agent.py
@@ -0,0 +1,69 @@
+import requests
+
+import socket
+
+import psutil
+
+import time
+
+
+
+SERVER_URL = "http://172.28.37.80:8000/api/metricas"
+
+
+
+while True:
+
+    hostname = socket.gethostname()
+
+
+    ip_local = socket.gethostbyname(hostname)
+
+
+    cpu = psutil.cpu_percent(interval=1)
+
+
+    ram = psutil.virtual_memory()
+
+
+    datos = {
+
+        "servidor": hostname,
+
+        "ip_local": ip_local,
+
+        "internet": "online",
+
+        "latencia_ms": 0,
+
+        "cpu_porcentaje": cpu,
+
+        "ram_porcentaje": ram.percent,
+
+        "ram_usada_gb": round(ram.used / (1024 ** 3), 2),
+
+        "ram_total_gb": round(ram.total / (1024 ** 3), 2)
+
+    }
+
+
+    try:
+
+        response = requests.post(
+
+            SERVER_URL,
+
+            json=datos
+
+        )
+
+
+        print(response.json())
+
+
+    except Exception as e:
+
+        print("Error:", e)
+
+
+    time.sleep(5)
